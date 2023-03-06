3b72d04c615547d341630bd7cca8be6942bf4449
EthanJ <e_jutila@fanshaweonline.ca>
2023-03-06 18:02:13 -0500

    idk

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
 // drum kit: press a key to play the sound. use JavaScript to retrieve the data-key attribute and then use that as a selector to find the matching audio file and play it!
+console.log('music player script file');
+let allDivs = document.querySelectorAll('.key');
+//listen for keyboard events happening in the browser window
+window.addEventListener('keyup', findMatchingAudio);
 
-(() => {
-    console.log('music player script file');
-    
-})();
+function findMatchingAudio(event) {
+    //debugger;
+    //try to find the matching audio element using the keyCode that we get from the event object
+
+    //find the matching div (the visual element on the screen) and animate that as well (add a bit of UX)
+    let targetAudio = document.querySelector(`audio[data-key="${event.keyCode}"]`),
+    targetDiv = document.querySelector(`div[data-key="${event.keyCode}"]`);
+
+    // the ! operator checks for a negative condition (it's the NOT operator)
+    if (!targetAudio) {return} // no point in running any JS if there's no audio element to play
+
+    targetAudio.currentTime = 0;
+    targetAudio.play();
+
+    //animate the div that matches (border, drop shadow etc)
+    targetDiv.classList.add('playing');
+
+    // data-key="65"
+}
+
+function resetKey() { this.classList.remove('playing'); }
+
+allDivs.forEach(key => key.addEventListener('transitioned', resetKey));
